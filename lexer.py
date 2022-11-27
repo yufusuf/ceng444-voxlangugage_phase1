fce68073333bed7a212ada31b9422912fff55d54
yufusuf <yusufsahin429@gmail.com>
2022-11-28 02:12:11 +0300

    lexer is completed

--- lexer.py
+++ lexer.py
@@ -6,19 +6,72 @@ class Lexer(sly.Lexer):
                EQ, LT, LE, GT, GE, NE, AND,
                FALSE, TRUE, FUN, FOR, OR,
                RETURN, VAR, STRING, NOT}
-
     #LE := <=
     #EQ := ==
     #LT := <
     #GT := >
     #GE := >=
     #NE := !=
-
+    #tabs and spaces etc
+    ignore = r' \s' 
+    ignore_comment = r'//.*'
     #Sanitize your tokens for the literals in the language (not token literals)!
     #each token t for NUMBER should have type(t.value) == float
     #each token t for STRING should have type(t.value) == str (remove the quotes!)
     #each token t for TRUE/FALSE should have type(t.value) == bool
+    ID = r'[a-zA-Z]\w*'
+    ID['if'] = IF 
+    ID['else'] = ELSE
+    ID['while'] = WHILE
+    ID['for'] = FOR
+    ID['or'] = OR
+    ID['and'] = AND
+    ID['fun'] = FUN
+    ID['return'] = RETURN
+    ID['var'] = VAR
+    ID['print'] = PRINT
 
+    PLUS = r'\+'
+    MINUS = r'-'
+    TIMES = r'\*'
+    DIVIDE = r'/'
+    EQ = r'=='
+    ASSIGN = r'='
+    LE = r'<='
+    GE = r'>='
+    NE = r'!='
+    LT = r'<'
+    GT = r'>'
+    NOT = r'!' 
+    
     literals = { '(', ')', '{', '}', '[', ']', ';' , ',', '#'}
 
-    #Do not modify the sets Lexer.literals and Lexer.tokens!
+    @_(r'(\d+[.])?\d+')
+    def NUMBER(self, t):
+        t.value = float(t.value)
+        return t
+
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += len(t.value)
+        return t
+
+    @_(r'true')
+    def TRUE(self, t):
+        t.value = bool(t.value)
+        return t
+    @_(r'false')
+    def FALSE(self, t):
+        t.value = bool(t.value)
+        return t
+
+    @_(r'"[^"]*"') 
+    def STRING(self, t):
+        t.value = str(t.value[1:len(t.value) - 1])
+        return t
+
+    def error(self, t):
+        # print('line : %d illegal character %r' % ((self.lineno), (t.value[0])))
+        self.index += 1
+        t.value = t.value[0]
+        return t
